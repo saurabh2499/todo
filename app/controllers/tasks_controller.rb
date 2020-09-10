05c2c03af4019aeea89a25440ddb30f80484084c
saurabh2499 <heysaurabh234@gmail.com>
2020-09-10 11:29:16 +0530

    listing of tasks done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,16 +2,20 @@ class TasksController < ApplicationController
 	
 
 	def index
-		@tasks = Task.all
+		@tasks = Task.order(priority: :desc, duedate: :asc)
 	end
 	def new
+		@task= Task.new
 	end
 
 	def create
 		@task = Task.new(task_params)
  
-  		@task.save
-  		redirect_to @task
+  		if @task.save
+  			redirect_to @task
+  		else
+  			render 'new'
+  		end
 	end
 
 	def show
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+	validates :title, presence: true
+	validates :text, presence: true, length: { minimum: 5 }
+	validates :priority, presence: true
+	validates :duedate, presence: true
 end
