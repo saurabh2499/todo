dd12df2bad257393cc9944f248d22d674d0ff9d0
saurabh2499 <heysaurabh234@gmail.com>
2020-09-10 12:47:13 +0530

    minute changes

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,4 @@
 class Task < ApplicationRecord
 	validates :title, presence: true
 	validates :text, presence: true, length: { minimum: 5 }
-	validates :priority, presence: true
-	validates :duedate, presence: true
 end
